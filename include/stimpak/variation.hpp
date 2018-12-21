ba1d8ca25e97da576b5d3febf221c843dc3f7ed9
superbr4in <15275491+superbr4in@users.noreply.github.com>
2018-12-21 11:31:02 +0100

    Merged filter and visitor headers.

--- include/stimpak/variation.hpp
+++ include/stimpak/variation.hpp
@@ -1,11 +1,21 @@
 #pragma once
 
-#include <tuple>
 #include <variant>
 #include <vector>
 
 namespace sti
 {
+    /*!
+     *  Helper type to provide multiple lambdas to the \c std::visit function.
+     */
+    template <typename... Ts>
+    struct generic_visitor : Ts...
+    {
+        using Ts::operator()...;
+    };
+    template <typename... Ts>
+    generic_visitor(Ts...) -> generic_visitor<Ts...>;
+
     /*!
      *  Transfers the values of a sequence containing diverse types to multiple sequences with
      *  each containing one certain type.
